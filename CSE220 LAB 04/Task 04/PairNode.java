9cd3aae3c45f3a2c41d43229af620a8f86ab3416
Mazid Zomader <mazidzomader.bracu@gmail.com>
2024-12-05 22:42:36 +0600

    Add files via upload

---
+++ CSE220 LAB 04/Task 04/PairNode.java
@@ -0,0 +1,13 @@
+public class PairNode {
+    
+    Integer key;
+    String value;
+    PairNode next;
+
+    public PairNode( Integer k, String v ){
+        // TO DO
+        this.key = k;
+        this.value = v;
+    }
+    
+}
